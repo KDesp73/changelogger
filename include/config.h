f3a68673933f1f1841bbec4d3a04f5304b78747f
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-11-18 23:39:25 +0200

    updated version to 0.0.20

--- include/config.h
+++ include/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 #define EXECUTABLE_NAME "changelogger"
-#define VERSION "0.0.19"
+#define VERSION "0.0.20"
 
 #define CHANGELOG_FILE "CHANGELOG.md"
 #define CHANGELOG_DIR ".changelog"
